ba3c219e33640a3659eb69fad500b94abfa0532b
田中緑(macbook) <evergreen.69rockn@gmail.com>
2021-04-05 07:36:03 +0900

    change：モーダルクローズ時にimgのデータも削除

--- src/App.vue
+++ src/App.vue
@@ -179,6 +179,7 @@ export default defineComponent({
       modalState.isActive = false;
       weather.geolocation = "";
       weather.location = "";
+      weather.img = "";
     };
 
     return {
